97f1a4c4162f0988e641fc7323e8a3ebdd0a0d92
Scott Umsted <scottumsted@gmail.com>
2016-10-02 17:58:52 -0500

    add proxy to carrier

---
+++ carrier/CarrierHelper_client.py
@@ -0,0 +1,30 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+from settings import Settings
+
+settings = Settings()
+
+
+def g(action, *args):
+    url = settings.CARRIER['ENDPOINT'] + '/' + action
+    for arg in args:
+        if arg is not None:
+            url += '/' + str(arg)
+    r = requests.get(url,
+                     auth=HTTPBasicAuth(settings.CARRIER['CARRIER_USERNAME'],
+                                        settings.CARRIER['CARRIER_PASSWORD'])
+                     )
+    return r.json()
+
+
+def CarrierHelper_which_carrier(tracking_number):
+    return g('CarrierHelper_which_carrier', tracking_number)['return_value']
+
+
+def CarrierHelper_track(tracking_number, carrier=None):
+    return g('CarrierHelper_track', tracking_number, carrier)['return_value']
+
+
+if __name__ == '__main__':
+    pass
---
+++ routes/carrier_api.py
@@ -0,0 +1,13 @@
+from bottle import get
+
+from carrier.CarrierHelper_client import CarrierHelper_track, CarrierHelper_which_carrier
+
+
+@get('/shipment/{tracking_number}')
+def get_shipment(tracking_number):
+    return CarrierHelper_track(tracking_number)
+
+
+@get('/carrier/{tracking_number}')
+def get_carrier(tracking_number):
+    return CarrierHelper_which_carrier(tracking_number)
