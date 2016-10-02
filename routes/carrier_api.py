b0f3a67d71b7106e7683ef42c19f42b71eb2bbf6
Scott Umsted <scottumsted@gmail.com>
2016-10-02 18:04:39 -0500

    add proxy to carrier

--- routes/carrier_api.py
+++ routes/carrier_api.py
@@ -3,11 +3,11 @@ from bottle import get
 from carrier.CarrierHelper_client import CarrierHelper_track, CarrierHelper_which_carrier
 
 
-@get('/track/{tracking_number}')
+@get('/track/<tracking_number>')
 def get_shipment(tracking_number):
     return CarrierHelper_track(tracking_number)
 
 
-@get('/carrier/{tracking_number}')
+@get('/carrier/<tracking_number>')
 def get_carrier(tracking_number):
     return CarrierHelper_which_carrier(tracking_number)
